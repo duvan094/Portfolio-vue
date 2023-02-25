0e2b3a695c96fd396f5e278cbe90660b2a77c28c
Jacob Duvander <jacobduvander@gmail.com>
2023-02-26 00:46:52 +0100

    setting up portfolio

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+  <footer>
+    Jacob Duvander
+  </footer>
+</template>
+
+<style scoped lang="scss">
+
+  header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+
+    .wrapper {
+      width: 100%;
+      max-width: 1080px;
+      margin: auto;
+      padding: 1rem;
+
+      nav {
+        display: flex;
+        gap: 1rem;
+      }
+    }
+  }
+
+  main {
+    margin: auto;
+    width: 100%;
+    max-width: 720px;
+    min-height: 720px;
+    display:flex;
+    align-items: center;
+  }
+
+  footer {
+    text-align: center;
+  }
+
+</style>
