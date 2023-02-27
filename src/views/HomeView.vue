115b1d99aa212863ce63fc854506ef482348a93e
Jacob Duvander <jacobduvander@gmail.com>
2023-02-27 20:40:16 +0100

    some typography changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,9 @@
 <template>
   <main>
     <section>
-      <h1>Welcome to my site!</h1>
-      <h4>I'm a frontend developer currently living in Jönköping, Sweden. This is just a site for fun.</h4>
-      <p>It is still a work in progress but I will use this site to gather links to hobby projects I've been working on.</p>
+      <h1>Welcome to my&nbsp;site!</h1>
+      <h4>I'm a frontend developer currently living in Jönköping, Sweden. This is just a site for&nbsp;fun.</h4>
+      <p>The website is still a work in progress but I will use it for gathering links to hobby projects I've been working on.</p>
     </section>
     <section>
       <h2>Projects</h2>
