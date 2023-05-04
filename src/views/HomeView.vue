b6d4572d9057b15f48652b5ca3c555db667d1b61
Jacob Duvander <jacobduvander@gmail.com>
2023-05-04 23:19:22 +0200

    added links to old projects

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,50 @@ import SlideShow from '../components/SlideShow.vue'
     </section>
     <section>
       <h2>Projects</h2>
-      <ExternalLink title="AoE2 Soundboard" link="https://aoe2.jacobduvander.se"/>
+      <p>Here are a few selected hobby projects. After I graduated and started working full-time as a web developer I had a long break from hobby projects, hopefully I'll have more time in the future.</p>
+      <div class="link-container">
+        <h4>2023</h4>
+        <div class="link-group">
+          <p><ExternalLink title="AoE2 Soundboard" link="https://aoe2.jacobduvander.se"/></p>
+        </div>
+        <h4>2019</h4>
+        <div class="link-group">
+          <p><ExternalLink title="Wasted time" link="https://wastedtime.jacobduvander.se"/></p>
+        </div>
+        <h4>2018</h4>
+        <div class="link-group">
+          <ExternalLink title="Quiz" link="https://quiz.jacobduvander.se/"/>
+          <ExternalLink title="Coffee infographic" link="https://coffee.jacobduvander.se"/>
+          <ExternalLink title="Swiss design poster" link="https://swiss.jacobduvander.se"/>
+        </div>
+        <h4>2017</h4>
+        <div class="link-group">
+          <p><ExternalLink title="Christmas bird" link="https://christmasbird.jacobduvander.se/"/></p>
+        </div>
+      </div>
     </section>
   </main>
 </template>
+
+<style lang="scss" scoped>
+
+.link-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+.link-group {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  gap: 1rem;
+  margin-bottom: 1rem;
+
+  @media(min-width: 720px) {
+    gap: 2rem;
+  }
+}
+
+</style>
