382cafeeb65d30db253427ded15d5b5e513587c5
Jacob Duvander <jacobduvander@gmail.com>
2023-05-06 15:36:43 +0200

    added go back link

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
@@ -6,6 +7,7 @@
     <section>
       <h1 tabindex="-1" id="welcome">404 - Page not found :(</h1>
       <p>We couldn't find the page you were looking for...</p>
+      <p><RouterLink tabindex="0" to="/">Go back to the start page</RouterLink></p>
     </section>
   </main>
 </template>
