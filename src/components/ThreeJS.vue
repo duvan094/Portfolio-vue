571b1ce038ec916ee252b711903d1ea3e5aa76a2
Jacob Duvander <jacobduvander@gmail.com>
2023-05-19 18:46:25 +0200

    added camera

---
+++ src/components/ThreeJS.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="container" ref="container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+const container = ref(null)
+
+let scene, model, camera, renderer, controls
+
+const meshFile2 =  'camera.glb'
+
+function initateRenderer() {
+  scene = new THREE.Scene()
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  renderer = new THREE.WebGLRenderer( { antialias: true, logarithmicDepthBuffer: true, alpha: true } )
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  container.value.appendChild(renderer.domElement)
+
+  window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  })
+}
+
+
+function addLights() {
+  const ambientLight = new THREE.AmbientLight( 0xffffff, .3 );
+  scene.add(ambientLight)
+
+  const directionalLight = new THREE.DirectionalLight( 0xffffff, .5 );
+  scene.add( directionalLight )
+
+  const pointLight = new THREE.PointLight(0xffffff, 1)
+  pointLight.position.x = 20
+  pointLight.position.y = 50
+  pointLight.position.z = 20
+  scene.add(pointLight)
+
+  const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, .5 );
+  scene.add( light );
+
+  scene.fog = new THREE.FogExp2( 0xffffff, .06 );
+}
+
+async function loadModel() {
+  const loader = new GLTFLoader();
+
+  await new Promise((resolve) => { 
+    loader.load( meshFile2, function ( gltf ) {
+      model = gltf.scene
+      scene.add( model );
+      return resolve()
+
+    }, undefined, function ( ) {
+      return
+    });
+  })
+
+  model.position.y = 1
+}
+
+function animate() {
+  requestAnimationFrame(function () {
+    animate()
+  })
+
+  if(model) {
+    model.rotation.y += 0.005 
+  }
+
+  renderer.render(scene, camera)
+}
+
+onMounted(async () => {
+  initateRenderer()
+  addLights()
+  await loadModel()
+
+  controls = new OrbitControls( camera, renderer.domElement );
+  controls.enableDamping = true;
+  controls.minDistance  = 4
+  controls.maxDistance = 50;
+
+  camera.position.z = 4
+  camera.position.x = 3
+  camera.position.y = 2
+  controls.update();
+
+  animate()
+})
+
+</script>
+
+<style scoped>
+
+  .container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 0;
+    cursor: grab;
+  }
+
+  .container:active {
+    cursor: grabbing;
+  }
+
+</style>
--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,17 +1,30 @@
 <script setup>
 import { RouterLink } from 'vue-router'
+import ThreeJs from '../components/ThreeJS.vue'
 </script>
 
 <template>
   <main>
     <section>
-      <h1 tabindex="-1" id="welcome">404 - Page not found :(</h1>
-      <p>We couldn't find the page you were looking for...</p>
-      <p><RouterLink tabindex="0" to="/">Go back to the start page</RouterLink></p>
+      <ThreeJs />
+      <div class="text-404">
+        <h1 tabindex="-1" id="welcome">404 - Page not found :(</h1>
+        <p>We couldn't find the page you were looking for...</p>
+        <p><RouterLink tabindex="0" to="/">Go back to the start page</RouterLink></p>
+      </div>
     </section>
   </main>
 </template>
 
 <style lang="scss" scoped>
+
+  .text-404 {
+    margin-top: 15rem;
+
+    @media(min-width: 720px) {
+      margin-top: 20rem;
+    }
+
+  }
 
 </style>
