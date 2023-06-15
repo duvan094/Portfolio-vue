3c614545ef5347ea73bb0565cfd9aee251a19e68
Jacob Duvander <duvan.der@live.se>
2023-06-16 01:06:19 +0200

    form styling

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -11,35 +11,63 @@
     let email = ref("")
     let message = ref("")
 
+    let emailError = ref(false)
+    let messageError = ref(false)
+
+    function clearError(field) {
+        console.log('clear', field)
+
+
+        if(field === 'email') {
+            emailError.value = false
+        }
+
+        if(field === 'message') {
+            messageError.value = false
+        }
+    }
+
     async function sendEmail(event) {
         event.preventDefault()
-        console.log('sendEmail')
 
-        console.log('email', email.value)
-        console.log('message', message.value)
+        let error = false
+
+        if(!email.value) {
+            emailError.value = true
+            error = true
+        }
+
+        if(!message.value) {
+            messageError.value = true
+            error = true
+        }
+
+        if(error) {
+            return
+        }
 
         const data = {
             email: email.value,
             message: message.value
         }
 
-        await fetch('https://jacobduvander.se/.netlify/functions/sendEmail', {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, *cors, same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: "same-origin", // include, *same-origin, omit
-            headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: "follow", // manual, *follow, error
-            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
-        }).then((response)=>{
-            console.log(response)
-        }).catch((error)=>{
-            console.log(error)
-        })
+        // await fetch('https://jacobduvander.se/.netlify/functions/sendEmail', {
+        //     method: "POST", // *GET, POST, PUT, DELETE, etc.
+        //     mode: "cors", // no-cors, *cors, same-origin
+        //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        //     credentials: "same-origin", // include, *same-origin, omit
+        //     headers: {
+        //     "Content-Type": "application/json",
+        //     // 'Content-Type': 'application/x-www-form-urlencoded',
+        //     },
+        //     redirect: "follow", // manual, *follow, error
+        //     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        //     body: JSON.stringify(data), // body data type must match "Content-Type" header
+        // }).then((response)=>{
+        //     console.log(response)
+        // }).catch((error)=>{
+        //     console.log(error)
+        // })
 
     }
 
@@ -50,16 +78,16 @@
     <section v-else>
         <h2>Contact me!</h2>
         <form @submit.prevent="sendEmail" class="form">
-            <div class="input-field">
-                <input type="email" id="email" name="email" placeholder=" " v-model="email"/>
+            <div class="input-field" :class="{'error': emailError}">
                 <label for="email">Your email</label>
+                <input type="email" id="email" name="email" placeholder="ex. jacobduvander@gmail.com" v-model="email" @input="clearError('email')"/>
             </div>
-            <div class="input-field">
-                <textarea type="text" id="message" name="message" placeholder=" " v-model="message"/>
+            <div class="input-field" :class="{'error': messageError}">
                 <label for="message">Your message</label>
+                <textarea type="text" id="message" name="message" placeholder="ex. Hello! Nice website!" v-model="message" @input="clearError('message')"/>
             </div>
             <button type="submit" class="button">
-                Submit
+                Send message
             </button>
         </form> 
     </section>
@@ -76,22 +104,51 @@
         max-width: 420px;
         width: 100%;
         margin: auto;
+        align-items: flex-start;
     }
 
     input, textarea {
         padding: .5rem;
-        border-radius: 5px;
+        border-radius: 10px;
         background-color: var(--input-background);
-        border: 2px solid var(--link-color);
-        box-shadow: var(--box-shadow);
+        border: 3px solid var(--link-color);
+        box-shadow: var(--box-shadow-dark-theme);
         font-family: 'Figtree', sans-serif;
-        transition: .2s ease-out border-color;
+        // transition: .2s ease-out border-color;
         font-size: 1.2em;
         width: 100%;
     }
 
+    textarea {
+        min-width: 100%;
+        min-height: 100px;
+    }
+
+.input-field {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin-bottom: 2rem;
+
+    label {
+        display: inline-block;
+        font-weight: 600;
+        margin-bottom: .5rem;
+        color: var(--input-label-color);
+    }
+
+    &.error {
+        input, textarea {
+            border-color: #bb0000;
+        }
+    }
+
     input:focus, textarea:focus {
-        border-color: var(--link-color-hover);
+        border-color: var(--link-color);
+        border-color: var(--input-focus-color);
+        outline: 3px solid var(--input-focus-color-border);
+
     }
 
     input:focus + label, textarea:focus + label {
@@ -102,21 +159,9 @@
         transform: translate(0,-1.5rem) scale(0.9);
     }
 
-.input-field {
-    position: relative;
-    width: 100%;
-    margin-bottom: 2rem;
 
-    label {
-        position: absolute;
-        top: 0;
-        left: 0;
-        transform: translate(.7rem,.7rem);
-        transition: transform .2s ease-out;
-        z-index: 1;
-        font-weight: 600;
-    }
 }
+
 
 
 </style>
