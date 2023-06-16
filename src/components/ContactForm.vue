f23b62d55ba40ac37603b0bdf13cf3c5c4793695
Jacob Duvander <duvan.der@live.se>
2023-06-16 11:45:30 +0200

    changes to form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -14,9 +14,14 @@
     let emailError = ref(false)
     let messageError = ref(false)
 
+    let emailSent = ref(false)
+    let errorSending = ref(false)
+
+
     function clearError(field) {
         console.log('clear', field)
-
+        emailSent.value = false
+        errorSending.value = false
 
         if(field === 'email') {
             emailError.value = false
@@ -51,23 +56,27 @@
             message: message.value
         }
 
-        // await fetch('https://jacobduvander.se/.netlify/functions/sendEmail', {
-        //     method: "POST", // *GET, POST, PUT, DELETE, etc.
-        //     mode: "cors", // no-cors, *cors, same-origin
-        //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        //     credentials: "same-origin", // include, *same-origin, omit
-        //     headers: {
-        //     "Content-Type": "application/json",
-        //     // 'Content-Type': 'application/x-www-form-urlencoded',
-        //     },
-        //     redirect: "follow", // manual, *follow, error
-        //     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        //     body: JSON.stringify(data), // body data type must match "Content-Type" header
-        // }).then((response)=>{
-        //     console.log(response)
-        // }).catch((error)=>{
-        //     console.log(error)
-        // })
+        await fetch('https://jacobduvander.se/.netlify/functions/sendEmail', {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        }).then((response)=>{
+            // console.log(response)
+            emailSent.value = true
+        }).catch((error)=>{
+            console.log(error)
+            errorSending.value = true
+        })
+
+        
 
     }
 
@@ -76,7 +85,8 @@
 <template>
     <template v-if="!showForm"></template>
     <section v-else>
-        <h2>Contact me!</h2>
+        <h2>Send me a message ✉️</h2>
+        <p>Feel free to send me a message, I'll reply as soon as possible.</p>
         <form @submit.prevent="sendEmail" class="form">
             <div class="input-field" :class="{'error': emailError}">
                 <label for="email">Your email</label>
@@ -86,17 +96,31 @@
                 <label for="message">Your message</label>
                 <textarea type="text" id="message" name="message" placeholder="ex. Hello! Nice website!" v-model="message" @input="clearError('message')"/>
             </div>
-            <button type="submit" class="button">
-                Send message
-            </button>
+            <div class="button-container">
+                <button type="submit" class="button">
+                    Send message
+                </button>
+                <div v-if="emailSent" class="message-sent">
+                    <span>Message sent!</span>
+                </div>
+                <div v-else-if="errorSending" class="message-sent">
+                    <span>Something went wrong :(</span>
+                </div>
+            </div>
+
         </form> 
     </section>
 </template>
 
 <style scoped lang="scss">
-    section h2 {
-        text-align: center;
-        margin-bottom: 2rem;
+    section {
+        
+        margin-top: 6rem;
+
+        h2, p {
+            text-align: center;
+            margin-bottom: 2rem;
+        }
     }
     .form {
         display: flex;
@@ -105,6 +129,18 @@
         width: 100%;
         margin: auto;
         align-items: flex-start;
+    }
+
+    .button-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 1rem;
+
+        .message-sent span{
+            font-weight: 600;
+            color: var(--text-color);
+        }
     }
 
     input, textarea {
@@ -141,7 +177,29 @@
     &.error {
         input, textarea {
             border-color: #bb0000;
+
+            animation: error .3s ease-in-out 1;
+
         }
+
+        @keyframes error {
+            0% {
+                transform: translateX(0);
+            }
+            25% {
+                transform: translateX(-20px);
+            }
+            25% {
+                transform: translateX(20px);
+            }
+            75% {
+                transform: translateX(-20px);
+            }
+            100% {
+                transform: translateX(0);
+            }
+        }
+
     }
 
     input:focus, textarea:focus {
